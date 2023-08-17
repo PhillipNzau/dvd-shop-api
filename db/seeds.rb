04cbe05ac6450923a6c87a61677e4c91ce6c8aaa
phillipnzau <nzauonline@gmail.com>
2023-08-17 20:27:14 +0300

    Movies listing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# Clear existing data
+Movie.destroy_all
+
+# Seed data
+movies = [
+  {
+    title: "Movie Title 1",
+    year: 2020,
+    length: 120,
+    description: "A great movie.",
+    poster_url: "https://example.com/poster1.jpg",
+    category: "Action",
+    discount: false,
+    female_director: true
+  },
+  {
+    title: "Movie Title 2",
+    year: 2018,
+    length: 105,
+    description: "An exciting film.",
+    poster_url: "https://example.com/poster2.jpg",
+    category: "Adventure",
+    discount: true,
+    female_director: false
+  },
+  # Add more movie entries as needed
+]
+
+movies.each do |movie_attributes|
+  Movie.create!(movie_attributes)
+end
